4a16f14623d9b69394f1ccd3a28ddb84a7a59a67
meetesh06 <meeteshmehta4@gmail.com>
2023-02-17 01:22:59 +0530

    add basepath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+import basePath from "./basePath.json"
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true
+  trailingSlash: true,
+  basePath
 }
 
 module.exports = nextConfig
